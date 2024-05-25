391d0c0811186e7b863f2d66f26f625a5aed56b3
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-25 18:00:35 +0300

    second commit

---
+++ 2.Javascript Dahil etme/app2.js
@@ -0,0 +1,1 @@
+console.log("metehan mercan");
---
+++ 3.Cıktı verme/app.js
@@ -0,0 +1,24 @@
+// Document write ile cıktı verme
+
+document.writeln("hello world <br>");  //!  <br> alt satıra geçirir 
+document.write("Metehan mercan <br>");
+document.write("25.05.2024");
+
+
+// console log ile cıktı verme 
+
+console.log("HELLO WORLD");
+console.log("METEHAN MERCAN");
+console.log("25.05.2024");
+console.log(true);
+console.log({name:"mete",surname:"mercan"});
+
+let a=5;
+let b=10;
+console.log(a+b);
+
+
+// alert (uyarı) popup ile cıktı verme 
+
+console.log(window);
+alert("MERHABAA ")
