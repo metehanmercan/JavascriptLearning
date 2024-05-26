500b94f59f492ae08f32663dae40b7c722b676ef
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-26 14:11:06 +0300

    dialogue boxes

--- 6.DialogueBoxes/app.js
+++ 6.DialogueBoxes/app.js
@@ -1,8 +1,17 @@
 /*
 ----------diyalog kutuları--------
 !   alert
-!   prompt
-!   confirm
+!   prompt ---- scanner sınıfına benzer  kullanıcıdan alınan değer string döner 
+!   confirm 
 */
 
 alert("dikkat");
+
+let namee=prompt("isminizi giriniz");
+let age=prompt("yaşınızı giriniz");  
+console.log("isminiz : " +namee);
+console.log("yaşınız : "+ age);
+
+
+let sunuc=confirm("silmek istediğinize emin misiniz? ");
+console.log(sunuc);
