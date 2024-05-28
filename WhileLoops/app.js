77c11c78831f1d9302cf88200c0051f2cec16794
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-28 17:22:24 +0300

    while loop

---
+++ WhileLoops/app.js
@@ -0,0 +1,15 @@
+
+let a = 1;
+while (a <= 12) {
+    console.log(a);
+    a++;
+}
+console.log("------")
+
+let b = 1;
+while (b <= 10) {
+    if (b % 2 == 0) {
+        console.log(b)
+    }
+    b++;
+}
