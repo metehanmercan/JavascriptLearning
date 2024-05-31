69db89e0bb7c811539990a0641e4fdbdc5be0b8c
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-31 13:54:31 +0300

    math sınıfı

---
+++ MathClass/app.js
@@ -0,0 +1,33 @@
+// Math sınıfı metotları 
+let number=3.15;
+
+// flor noktadan sonrasını atar 
+console.log(Math.floor(number)); 
+
+// ceil  bir üst tam sayıya yuvarlar 
+console.log(Math.ceil(number)); 
+
+// round  en yakın tam sayıya yuvarlar
+console.log(Math.round(number));
+
+// max min verilen arraydeki  en büyük ve en küçük değerleri döner
+console.log(Math.max(1,2,3,4,5,6,65));
+console.log(Math.min(1,2,3,4,5,6,65));
+
+// abs mutlak değer alır 
+let a=-5;
+console.log(Math.abs(a));
+
+// sqrt karekökü alır
+let b=36;
+console.log(Math.sqrt(b));
+
+// pow üs alır
+console.log(Math.pow(2,4));
+
+// PI 
+console.log(Math.PI);
+
+// random 
+console.log(Math.floor(Math.random()*100));
+
