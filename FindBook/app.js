64fc92371294b39e196e018a8ef6388a90e250b9
metehanmercan <mthnmrcn0133@gmail.com>
2024-05-31 12:37:30 +0300

    find book update

--- FindBook/app.js
+++ FindBook/app.js
@@ -5,7 +5,7 @@ let kitap3 = { isim: "İnsan Neyle Yaşar", yazar: "Tolstoy", fiyat: 34, raf: "3
 let kitap4 = { isim: "Zafer sızlanarak kazanılmaz", yazar: "Haluk Tatar", fiyat: 45, raf: "4.1 RAF" }
 let kitap5 = { isim: "Şeker Portakalı", yazar: "Jose Mauro de Vasconcelos", fiyat: 22, raf: "5.3 RAF" }
 
-let kitaplat = [kitap1, kitap2, kitap3, kitap4, kitap5];
+let kitaplar = [kitap1, kitap2, kitap3, kitap4, kitap5];
 
 let raf11 = { kod: "1.1 RAF", goster: false }
 let raf12 = { kod: "1.2 RAF", goster: false }
@@ -38,7 +38,7 @@ let raf54 = { kod: "5.4 RAF", goster: false }
 let raf55 = { kod: "5.5 RAF", goster: false }
 
 let raflar = [
-    [raf51, raf52, raf53, raf54, raf54],
+    [raf51, raf52, raf53, raf54, raf55],
     [raf41, raf42, raf43, raf44, raf45],
     [raf31, raf32, raf33, raf34, raf35],
     [raf21, raf22, raf23, raf24, raf25],
@@ -46,14 +46,51 @@ let raflar = [
 ];
 
 function rafOlustur() {
-
-    let satir = "";
+    console.clear();
+    let satir ="";
     for (let i = 0; i < raflar.length; i++) {
         for (let j = 0; j < 5; j++) {
-            satir += "|" + (raflar[i][j].goster ? raflar[i][j].kod : "---|")+"|";
+            satir += "|" + (raflar[i][j].goster ? raflar[i][j].kod : "---") + "";
         }
         console.log(satir);
+        console.log("--------------------");
         satir = "";
     }
 }
+
+function kodBul(kitapIsmi) {
+    let rafKod = null;
+    kitaplar.forEach(function (kitap) {
+        if (kitap.isim.toUpperCase().includes(kitapIsmi.toUpperCase(), 0)) {
+            rafKod = kitap.raf;
+        }
+    });
+    return rafKod;
+}
+
+function raftaGoster(rafKodu) {
+    for (let i = 0; i < raflar.length; i++) {
+        for (let j = 0; j < 5; j++) {
+            if (raflar[i][j].kod == rafKodu) {
+                raflar[i][j].goster = true;
+                break;
+            }
+        }
+    }
+}
+
 rafOlustur();
+
+let kitapIsmi = prompt("kitap ismi giriniz. ");
+let rafKod = kodBul(kitapIsmi);
+
+
+if (rafKod != null) {
+    raftaGoster(rafKod);
+    rafOlustur();
+   
+   
+} else {
+    alert("kitap kütüphanemizde yok");
+    console.log("Kitap kütüphanede bulunamadı.");
+}
